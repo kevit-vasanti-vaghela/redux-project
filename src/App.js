39baa9e38f7b913251bb65a256f3813ca82736e8
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-02 17:15:15 +0530

    async code of http request in function that created in cart slice

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 
 let isInitial = true;
@@ -16,45 +16,12 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      }))
-      const response = await fetch(
-        'https://custom-hook-and-http-default-rtdb.firebaseio.com/cart.json', 
-        { 
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      )
-      if (!response.ok) {
-        throw new Error ('Sending cart data failed.')
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!',
-      }))
-    }
 
     if (isInitial) {
       isInitial = false;
       return;
     }
-    
-    sendCartData().catch(error => {
-      dispatch (
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Something went wrong!'
-        })
-      )
-    })
-
+    dispatch(sendCartData(cart));
   },[cart,dispatch])
   return (
     <Fragment>
